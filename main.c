edbfdb22ccfd01e8f308c729fe580e7cae88eff4
Abdulrahman Ismael <116180351+AbdulrahmanIsmael@users.noreply.github.com>
2025-08-03 18:38:44 +0300

    release v1.0

---
+++ main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+// Functions Prototypes
+int getRandomNumber(int max, int min);
+int leaveOrTryAgain();
+
+int main()
+{
+    // random number to guess
+    int min = 1;
+    int max = 20;
+    int correctGuess = getRandomNumber(max, min);
+
+    // user input: the guess
+    int guess = 0;
+
+    // process the guess
+    while (correctGuess != guess)
+    {
+
+        // read user input
+        printf("Enter Your Guess: ");
+        scanf("%d", &guess);
+
+        if (correctGuess == guess)
+        {
+            printf("Congratulations, your guess was correct!\n\n");
+        }
+        else if (guess - 1 == correctGuess || guess + 1 == correctGuess)
+        {
+            printf("You are very NEAR to the correct answer!\n\n");
+        }
+        else if (guess > correctGuess)
+        {
+            printf("Too HIGH!\n\n");
+        }
+        else if (guess < correctGuess)
+        {
+            printf("Too LOW!\n\n");
+        }
+    }
+
+    // user decision to leave the game or play again
+    int userLeaveOrPlay = leaveOrTryAgain();
+    if (userLeaveOrPlay == 1)
+    {
+        main();
+    }
+    else
+    {
+        printf("See you later! Goodbye.\n");
+    }
+
+    return 0;
+}
+
+// Functions
+int getRandomNumber(int max, int min)
+{
+    srand(time(0));
+    int correctNumber = (rand() % (max - min + 1)) + min;
+    return correctNumber;
+}
+
+int leaveOrTryAgain()
+{
+    char choice = '\0';
+    printf("Thank you for playing, would you like to leave or play again (Y for Yes or N for No)? ");
+    scanf(" %c", &choice);
+    return choice == 'Y' || choice == 'y' ? 1 : 0;
+}
